e9d5b8e0233e9ddc58709d350c7dfdba401b8d24
Lubomir Naydenov <lubonay@gmail.com>
2018-02-03 20:54:12 +0200

    Added RxJava dependencies and a base fragment that handles disposing of observables in onStop.

---
+++ app/src/main/java/com/npace/rptlog/BaseFragment.kt
@@ -0,0 +1,21 @@
+package com.npace.rptlog
+
+import android.support.v4.app.Fragment
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+/**
+ * Created by lubo on 2/3/2018.
+ */
+open class BaseFragment : Fragment() {
+    private val disposable = CompositeDisposable()
+
+    override fun onStop() {
+        disposable.clear()
+        super.onStop()
+    }
+
+    fun addSubscription(subscription: Disposable) {
+        disposable.add(subscription)
+    }
+}
